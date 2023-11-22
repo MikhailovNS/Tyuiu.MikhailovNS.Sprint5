33a45386eeca541842ecf21e951bd7d5e0d041b2
MikhailovNS <n.s.mikhailov@gmail.com>
2023-11-23 00:03:15 +0500

    Task5

---
+++ Tyuiu.MikhailovNS.Sprint5.Task5.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MikhailovNS.Sprint5.Task5.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.MikhailovNS.Sprint5.Task5.V20.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a69f35b3-43c4-4d7e-a5c0-328421ab297d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MikhailovNS.Sprint5.Task5.V20/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.MikhailovNS.Sprint5.Task5.V20.Lib;
+
+namespace Tyuiu.MikhailovNS.Sprint5.Task5.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                *");
+            Console.WriteLine("* Тема: Обработка файлов                                                   *");
+            Console.WriteLine("* Задание #5                                                               *");
+            Console.WriteLine("* Вариант #20                                                              *");
+            Console.WriteLine("* Выполнил: Михайлов Никита Станиславович | АСОиУБ-23-1                    *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Дан файл С:/DataSprint5/InPutDataFileTask5V20.txt (файл взять из архива  *");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С:/DataSprint5/ и       *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть вещественное значение. Найти      *");
+            Console.WriteLine("* среднее значение всех целых чисел в файле, которые находятся в           *");
+            Console.WriteLine("* промежутке от -10 до 10. Полученный результат вывести на консоль.        *");
+            Console.WriteLine("* У вещественных значений округлить до трёх знаков после запятой.          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            string path = @"C:\Sprint5\InPutDataFileTask5V20.txt";
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine("Данные хранятся в файле" + path);
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Среднее арифметическое всех чисел в промежутке (-10;10) равно: "+ res);
+            Console.ReadKey();
+        }
+    }
+}
